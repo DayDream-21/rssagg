e78d1f94bc5808ce565dc1ae413830fc0d658b5e
Slava Mashkov <mashkov.vd@gmail.com>
2023-11-19 18:22:15 +0300

    Read properties from .env file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/DayDream-21/rssagg
 
 go 1.21.3
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+const envFileName string = ".env"
+const portKey string = "PORT"
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load(envFileName)
+	if err != nil {
+		log.Fatalf("Error loading %s file\n", envFileName)
+	}
+
+	port := os.Getenv(portKey)
+	if port == "" {
+		log.Fatalf("'%s' environment variable is not set\n", portKey)
+	}
+
+	fmt.Printf("Port: %s\n", port)
 }
